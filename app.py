9ebf2b745994d7c1ac3d11711685736f20d1749e
Katja Parakhnevich <katjaparakhnevich@mac.home>
2025-04-20 18:51:10 +0200

    updated dash layout

--- app.py
+++ app.py
@@ -13,14 +13,23 @@ app = Dash(__name__,external_stylesheets=[dbc.themes.QUARTZ])
 
 app.layout = dbc.Container([
     dbc.Row([
-        dbc.Col(html.H2("Welcome to the movie recommendation engine!", style={"textAlign": "center"}))]),
+        dbc.Col(
+            html.H2("Welcome to the movie recommendation engine!", style={"textAlign": "center"}))],  justify="center"),
     dbc.Row([
-        dbc.Col(dcc.Input(id="movie-title", type="text",
-                  placeholder="Please enter the movie title ", style={"width": "75vh", "height":"3vh"})),
-        dbc.Col(html.Button("Search", id="search-button", n_clicks=0, style={"height":"3vh", "border":"none", "text-align":"center"}))
-]),
-    dbc.Row(html.Div(id="movie-recommendation"))
-],style={"width": "250vh", "height": "20vh", "marginTop": "35vh", "marginLeft": ""})
+        dbc.Col(
+            dcc.Input(id="movie-title", type="text",
+                  placeholder="Please enter a movie title ", style={"width": "75vh", "height":"3vh"}), width="auto"),
+        dbc.Col(
+            html.Button("Search", id="search-button", n_clicks=0, style={"height":"3vh", "border":"none", "text-align":"center", "opacity": "0.6"}), width="auto")
+], justify="center"),
+    dbc.Row([
+        dbc.Col(
+            html.Div(id="movie-recommendation"),
+            width=12
+        )
+    ], justify="center")
+], style={"marginTop": "25vh"}, fluid=True)
+
 
 @callback(
     Output("movie-recommendation", "children"),
@@ -29,19 +38,31 @@ app.layout = dbc.Container([
 )
 def update_recommendations(n_clicks, movie_name):
    if n_clicks > 0:
+        if movie_name not in sim_df.index:
+                
+                return [dbc.Row([
+                dbc.Col(
+                    html.P(f"{movie_name} was not found", style={"textAlign": "center"}), width="auto"
+                )
+            ], justify="center")]
+
         if movie_name:
-            recommendations = get_recommendations(movie_name,sim_df, similarity_score, movies_df)
-            columns = [dbc.Col(rec) for rec in recommendations]
+            recommendations, covers = get_recommendations(movie_name,sim_df, similarity_score, movies_df)
 
-            return [html.Div([
-                dbc.Row(html.H6(" "), style={"height": "3vh"}),
-                dbc.Row(columns, style={"width": "100vh","margin": "", "marginTop": "3vh"})
-                ],style={"width": "100vh", "marginRight": "5vh"})
+            return [dbc.Row([
+                dbc.Col([
+                html.P(rec,style={"marginTop": "3vh", "whiteSpace": "nowrap", "textAlign": "center", "overflow": "hidden", "textOverflow": "ellipsis"}),
+                html.Img(src=cover, style={"marginTop": "5vh", "width":"45%", "height": "70%","textAlign": "center", "display": "block", "margin":"0 auto" }),
+                
+            ], style={"width": "150px"}) for rec, cover in zip(recommendations, covers)],justify="center",style={})]
 
-            ]
         else: 
-            return "Please enter a movie title!"
-
+            return [dbc.Row([
+                dbc.Col(
+                    html.P("Please enter a movie title!", style={"textAlign": "center"}), width="auto"
+                )
+            ], justify="center")]
+         
 
 
 if __name__ == "__main__":
